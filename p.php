204a3b9df92eb0d14974a76bd6099b5c3c7ed8b1
vlhay <bloggernamnguyen@gmail.com>
2018-12-15 17:07:16 +0700

    Update p.php

--- p.php
+++ p.php
@@ -52,7 +52,7 @@ if (isset($_GET['url']) && strstr($_GET['url'], 'xvideos.com') != null) {
     $url = $_GET['url'];
     $urll=json_decode(getXvideo($url))->mp4high;
     $urll =  str_replace('&','@',$urll);
-header('Location: http://xemlasuong.viwap.com/code/'.$urll.'');
+header('Location: http://xemlasuong.viwap.com/code/?url='.$urll.'');
   
 }
 ?><?php
