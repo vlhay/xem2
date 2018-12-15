d05d59a4d118dd740495d8e6228e78509211a368
vlhay <bloggernamnguyen@gmail.com>
2018-12-15 08:51:04 +0700

    Update tt.php

--- tt.php
+++ tt.php
@@ -160,10 +160,10 @@ $bai = strip_tags($bai,'<img>');
 $bai = preg_replace('#<img(.*?)src="(.*?)"(.*?)>#is','[img]$2[/img]',$bai);
 $bai = preg_replace('/<p>(Chap|Chương|Phần)(.*)<\/p>/i', '<p><b>$1$2</b></p>', $bai);
 //$bai = preg_replace('/(hentai24h.org|truyenvip)/i', 'thichtruyentranh.viwap.com', $bai);
-echo ' <form action="http://thichtruyentranh.viwap.com/manager/chap/'.$idt.'" method="post"> <div class="list-group-item">Truyen goc: <input type="text" name="idt" value="$idt" maxlength="300"></div>
-<div class="list-group-item">Nội dung: <textarea name="content" id="content" rows="25">'.$bai.'</textarea></div>
-<div class="list-group-item"><input type="checkbox" name="comment" value="1" checked> Cho phép bình luận</div>
-<div class="list-group-item"><center><button type="submit" class="btn btn-primary btn-block">Đăng bài</button></form></center></div>';
+echo ' <form action="http://thichtruyentranh.viwap.com/manager/chap/'.$idt.'" method="post"> Truyen goc: <input type="text" name="idt" value="$idt" maxlength="300">
+Nội dung: <textarea name="content" id="content" rows="25">'.$bai.'</textarea>
+<input type="checkbox" name="comment" value="1" checked> Cho phép bình luận
+<button type="submit" class="btn btn-primary btn-block">Đăng bài</button></form>';
 }
 curl_close($bv);
 
