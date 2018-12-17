62d7aab3185c6b4856c581d088a216d1ef65eec6
vlhay <bloggernamnguyen@gmail.com>
2018-12-17 14:20:24 +0700

    Update pp.php

--- pp.php
+++ pp.php
@@ -1,15 +1,17 @@
-
 <?php
-$url = $_GET['url'];
-$url =  str_replace('ooooo','?',$url);
-
-$curl = curl_init();
-curl_setopt ($curl, CURLOPT_URL, $url);
-curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
-$link = curl_exec($curl);
-$link = explode('<title>',$link);
-$link = explode('</title>',$link[1]);
-$link = trim($title[0]);
-
-echo ''.$link.'';
+$url = 'http://www.youporn.com/watch/13618549/holed-school-girl-jojo-kiss-caught-masturbating-with-anal-toys/'; ///Trang cần leech
+$contents = file_get_contents($url);
+$link240 = explode("240: '",$contents);
+$link240q = explode("',",$link240[1]);
+$link480 = explode("480: '",$contents);
+$link480q = explode("',",$link480[1]);
+$link720 = explode("720: '",$contents);
+$link720q = explode("',",$link720[1]);
+$l240 = $link240q[0];
+$l480 = $link480q[0];
+$l720 = $link720q[0];
+// show link ra
+echo '240 : '.$l240.'<br/>';
+echo '480 : '.$l480.'<br/>';
+echo '720 : '.$l720.'<br/>';
 ?>
