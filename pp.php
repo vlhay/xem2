6934ed776aed945f147fbcb01cae8000cc72945f
vlhay <bloggernamnguyen@gmail.com>
2018-12-17 14:14:49 +0700

    Update pp.php

--- pp.php
+++ pp.php
@@ -1,13 +1,19 @@
 
 <?php
+$urll = $_SERVER["HTTP_REFERER"];
+$urll =  str_replace('?url=','@url=',$urll);
+$urll =  str_replace('?','ooooo',$urll);
+$urll =  str_replace('@url=','?url=',$urll);
 $url = $_GET['url'];
+$url =  str_replace('ooooo','?',$url);
+
 $curl = curl_init();
 curl_setopt ($curl, CURLOPT_URL, $url);
 curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
 $link = curl_exec($curl);
-$link = explode('<title>',$title);
-$link = explode('</title>',$title[1]);
+$link = explode('<title>',$link);
+$link = explode('</title>',$link[1]);
 $link = trim($title[0]);
 
-echo ''.$title.'';
+echo ''.$link.'';
 ?>
