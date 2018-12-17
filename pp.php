7d2f2e67b9d5a8ef93c14469061b03599acc94e2
vlhay <bloggernamnguyen@gmail.com>
2018-12-17 14:48:16 +0700

    Update pp.php

--- pp.php
+++ pp.php
@@ -1,5 +1,5 @@
 <?php
-$url = 'https://fr.pornhub.com/view_video.php?viewkey=ph5bcc71f8663d3'; ///Trang cần leech
+$url = 'http://fr.pornhub.com/view_video.php?viewkey=ph5bcc71f8663d3'; ///Trang cần leech
 $contents = file_get_contents($url);
 echo '240 : '.$contents.'<br/>';
 ?>
