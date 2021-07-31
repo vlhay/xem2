59a997b803a355ef36056ecddbaae03a5cab49cd
vlhay <bloggernamnguyen@gmail.com>
2021-07-31 20:58:51 +0700

    Update autoht.php

--- autoht.php
+++ autoht.php
@@ -133,7 +133,9 @@ echo '
 <div class="list"><center><button type="submit" class="btn btn-primary btn-block"  id="okbaby" >Đăng bài</button></form></center></div>
 </div>
 
-
+<script language="javascript"> 
+document.getElementById("okbaby").click(); 
+</script>
 
 
  '; 
